c564c3cb27c86c6c381a14d46825834b7f54e3df
qudsiyashaikh <zinharkhan8@Gmail.com>
2026-03-29 11:15:38 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -81,8 +81,8 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key type (Taaki warnings na aayein)
